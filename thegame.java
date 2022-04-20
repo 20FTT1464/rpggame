a051df8a774c3557a4339c8e88c7307ddcd8f9c2
nur alif saifullah <20ftt1464@student.pb.edu.bn>
2022-04-21 00:23:37 +0800

    added clases stat statement

--- thegame.java
+++ thegame.java
@@ -33,7 +33,13 @@ class thegame {
         System.out.println("Starting new game");
         String CharName = "character";
 
-        while (true) {
+        // Set Modifiers to 100%, so able to stack mods
+        // Character Skill { HP, MP, STR, DEF, SC } # initial
+        int[] charSk = {5, 5, 5, 5, 5};
+        // modifier { HP, MP, STR, DEF, SC }
+        int[] skMod = {100, 100, 100, 100, 100};
+
+        while (true) { // character naming loop
             System.out.print("Please type in your character name: ");
             Scanner nameInput = new Scanner(System.in);
 
@@ -47,6 +53,35 @@ class thegame {
                 break;
             } 
         }
+
+        String nameInputcheck;
+        while (true) {
+            String wrong = "";
+            if (wrong.equals("wrong")) {
+                System.out.println("pls choose one of the classes.");    
+            }
+            System.out.println("Choose Character Class: ");
+            System.out.println("1. Knight ");
+            System.out.println("2. Mage ");
+            Scanner nameInput = new Scanner(System.in);
+            nameInputcheck = nameInput.nextLine(); 
+            if (nameInputcheck.equals("1")) {
+                System.out.println("+20% STR");
+                System.out.println("+20% DEF");
+                skMod[2] += 20;
+                skMod[3] += 20;
+                break;
+            } else if (nameInputcheck.equals("2")) {
+                System.out.println("+20% STR");
+                System.out.println("+20% DEF");
+                skMod[1] += 20;
+                skMod[4] += 20;
+                break;
+            } else {
+                wrong = "wrong";
+            }
+        }
+
 
         
         
