e5822b713795775a6a75c4da1bc411c2019ce127
Eiji <20ftt1464@student.pb.edu.bn>
2022-04-23 21:13:44 +0800

    added rolling sp + added CharSk Println

--- thegame.java
+++ thegame.java
@@ -9,7 +9,7 @@ class thegame {
         System.out.println("Welcome!");
         System.out.println("1. New Game");
         System.out.println("2. Load");
-        
+
         System.out.print("Choose Number to Continue: ");
 
         String emptyCheck = uinput.nextLine();
@@ -17,7 +17,7 @@ class thegame {
             System.out.println("Please type in one of the choices");
         } else {
             int nuinput = Integer.parseInt(emptyCheck);
-            
+
             if (nuinput == 1) { // Userinput 1 AKA New Game
                 newGame();
             } else if (nuinput == 2) { // Userinput 2 AKA load
@@ -35,9 +35,9 @@ class thegame {
 
         // Set Modifiers to 100%, so able to stack mods
         // Character Skill { HP, MP, STR, DEF, SC } # initial
-        int[] charSk = {5, 5, 5, 5, 5};
+        int[] charSk = { 5, 5, 5, 5, 5 };
         // modifier { HP, MP, STR, DEF, SC }
-        int[] skMod = {100, 100, 100, 100, 100};
+        int[] skMod = { 100, 100, 100, 100, 100 };
 
         while (true) { // character naming loop
             System.out.print("Please type in your character name: ");
@@ -48,49 +48,83 @@ class thegame {
 
             System.out.println("Are you satisfied with the name?");
             Scanner iConfirm = new Scanner(System.in);
-            
-            if(iConfirm.nextLine().equals("yes")){
+
+            if (iConfirm.nextLine().equals("yes")) {
                 break;
-            } 
+            }
         }
 
         String nameInputcheck;
         while (true) {
-            String wrong = "";
-            if (wrong.equals("wrong")) {
-                System.out.println("pls choose one of the classes.");    
+            while (true) {
+                // Class Choosing
+                System.out.println("Choose Character Class: ");
+                System.out.println("1. Knight ");
+                System.out.println("2. Mage ");
+                Scanner nameInput = new Scanner(System.in);
+                nameInputcheck = nameInput.nextLine();
+                // Knight
+                if (nameInputcheck.equals("1")) {
+                    System.out.println("+20% STR");
+                    System.out.println("+20% DEF");
+                    System.out.print("Are you sure? (yes/no): ");
+                    Scanner nameInputconfirm = new Scanner(System.in);
+                    if (nameInputconfirm.nextLine().equals("yes")) {
+                        break;
+                    }
+                    // Mage
+                } else if (nameInputcheck.equals("2")) {
+                    System.out.println("+20% STR");
+                    System.out.println("+20% DEF");
+                    System.out.print("Are you sure? (yes/no): ");
+                    Scanner nameInputconfirm = new Scanner(System.in);
+                    if (nameInputconfirm.nextLine().equals("yes")) {
+                        break;
+                    }
+                } else {
+                    System.out.println("pls choose one of the classes.");
+                }
             }
-            System.out.println("Choose Character Class: ");
-            System.out.println("1. Knight ");
-            System.out.println("2. Mage ");
-            Scanner nameInput = new Scanner(System.in);
-            nameInputcheck = nameInput.nextLine(); 
+
+            // Assigning the modifier
             if (nameInputcheck.equals("1")) {
-                System.out.println("+20% STR");
-                System.out.println("+20% DEF");
                 skMod[2] += 20;
                 skMod[3] += 20;
                 break;
             } else if (nameInputcheck.equals("2")) {
-                System.out.println("+20% STR");
-                System.out.println("+20% DEF");
                 skMod[1] += 20;
                 skMod[4] += 20;
                 break;
             } else {
-                wrong = "wrong";
+
             }
         }
 
+        // Allocatable skill point rolling mechanism
+        int roll = 0;
+        while (true) {
+            roll = (int) (Math.random() * ((20 - 1) + 1)) + 1;
+            System.out.println("Rolled sp is " + roll);
+            System.out.print("Are you satisfied with rolled sp? (yes/no): ");
+            Scanner rollconfirm = new Scanner(System.in);
+            if (rollconfirm.nextLine().equals("yes")) {
+                break;
+            }
+        }
 
-        
-        
+        System.out.println("Character Stats");
+        System.out.println("________________________________________________________________________________________________________________________________");
+        System.out.println("HP: " + charSk[0] + "  %" + (skMod[0]-100) + " = " + ((charSk[0]/100)*skMod[0]) + "");
+        System.out.println("Current Allocatable SP: " + roll);
+
+
 
     }
 
     static void loadGame() {
         System.out.println("Load saved games amongus");
     }
+
     public static void main(String[] args) {
         gameHomepage();
 
